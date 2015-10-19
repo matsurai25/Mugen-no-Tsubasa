93e6126cf0a3cce91b1885fbb9b9bf1a95c5370a
matsurai25 <souta.m825@gmail.com>
2015-10-20 02:45:01 +0900

    イラスト部分、2サビ前まで

--- util/CompDuplicate.jsx
+++ util/CompDuplicate.jsx
@@ -18,11 +18,7 @@
 // Aの歌詞だけ
 // 空を見上げ僕らは夢見た あの鳥のような翼を 夏の陽炎が君を消した あの空に行ってしまったの？ 難しい本片手に作る この空を飛べる翼を 雨上がりの空架かった虹 今君もどこかで見てる？
 
-// ユニーク化
-// 空を見上げ僕らは夢たあの鳥ような翼夏陽炎が君消しに行ってま？難い本片手作るこ飛べ雨り架か虹今もどで
-
 // forEach使えないので使えるように
-
 // Production steps of ECMA-262, Edition 5, 15.4.4.18
 // Reference: http://es5.github.com/#x15.4.4.18
 if ( !Array.prototype.forEach ) {
@@ -53,14 +49,17 @@ if ( !Array.prototype.forEach ) {
 
 
 var activeItem = app.project.activeItem;
-// var Lyrics = "空を見上げ僕らは夢たあの鳥ような翼夏陽炎が君消しに行ってま？残され手紙握り締め今決探すへ飛ぶんだ作かけ大と羽ばく時き叶ろち描い世界で待る無限乗せつ日み難本片こべ雨架虹もど思満走出広小高景色少辿着届気持荒狂折霞む意識響女声「解放ムゲンノツバサ」天使光纏包青下会え奇跡幻彼方".split('');
 var Lyrics = "空を見上げ僕らは夢見た あの鳥のような翼を 夏の陽炎が君を消した あの空に行ってしまったの？ 残された手紙握り締め 今決めた 君を探すため空へ飛ぶんだ 作りかけの翼が大空へと羽ばたく時 きっときっと叶うだろう 僕たちが描いた夢が 空の上の世界できっと君は待ってるから 無限の夢翼に乗せ いつの日か飛んでみせる 難しい本片手に作る この空を飛べる翼を 雨上がりの空架かった虹 今君もどこかで見てる？ 君への思いを握り締め さあ時は満ちた走り出せ 翼を広げ 小さなこの翼が大空へと今羽ばたく 高い高いこの景色を なあ君も見ていたんだろ？ 空の上の世界へあと少しで辿り着ける 無限の夢翼に乗せ 届けるよこの気持ちを 荒れ狂う空 折れた翼 僕は空を飛べないの？ 霞む意識に響いた少女の声 「解き放て ムゲンノツバサ」 天使のような翼が 光纏い僕を包む 青い青い空の下で また君に出会えた奇跡 夢を乗せた翼が たとえ幻だとしても 夢幻の彼方の世界へ 君となら飛んで行ける".split('');
 
-// var letters = Lyrics.filter(function (x, i, self) {
-//   return self.indexOf(x) === i;
-// });
+var prefix = "borders_";
+var suffix = "_dark";
+
+
 var letters = [];
 var flag;
+var prefix = "borders_";
+var suffix = "_dark";
+
 letters.push(Lyrics[0]);
 
 for(var i = 0; i < Lyrics.length; i++){
@@ -81,7 +80,7 @@ if ((activeItem == null) || !(activeItem instanceof CompItem)) {
 }else{
   letters.forEach(function(_){
     var curComp = activeItem.duplicate();
-    curComp.name = "borders_"+_+"_dark";
+    curComp.name = prefix+_+suffix;
   })
 }
 
